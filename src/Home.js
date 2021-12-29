5eb9aa5e217f3f8fdc3c5158a32663133fdd1bce
Martial Séron <martial.seron@gmail.com>
2021-12-29 06:21:15 +0000

    add inspiring-image

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+import './Home.css';
 import HeaderBar from './HeaderBar';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
@@ -6,8 +7,8 @@ function Home() {
   return (
     <>
       <HeaderBar />
-      <main className="my-5 grid grid-cols-1 md:grid-cols-2">
-        <div className="tenant px-4 sm:px-6 md:px-8 py-10 sm:py-20 xl:py-24 text-center">
+      <main className="grid grid-cols-1 my-5 md:grid-cols-2">
+        <div className="px-4 py-10 text-center tenant sm:px-6 md:px-8 sm:py-20 xl:py-24">
           <h1 className="mt-0 mb-8 text-5xl font-extrabold leading-none tracking-tight md:text-6xl lg:text-7xl sm:mt-0 sm:mb-10">
             <span className="block text-primary">Je recherche</span>
             <span className="block text-secondary">un logement</span>
@@ -34,10 +35,10 @@ function Home() {
       </main>
 
       <div className="py-12 bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="lg:text-center">
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-secondary sm:text-4xl">Simplifiez vous l&apos;immobilier</p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            <p className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-secondary sm:text-4xl">Simplifiez vous l&apos;immobilier</p>
+            <p className="max-w-2xl mt-4 text-xl text-gray-500 lg:mx-auto">
               Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.
             </p>
           </div>
@@ -46,13 +47,13 @@ function Home() {
             <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
               <div className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-sm bg-ternary text-white">
+                  <div className="absolute flex items-center justify-center w-12 h-12 text-white rounded-sm bg-ternary">
                     {/* Heroicon name: outline/globe-alt */}
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Gagnez du temps</p>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Gagnez du temps</p>
                 </dt>
                 <dd className="mt-2 ml-16 text-base text-gray-500">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
@@ -62,9 +63,9 @@ function Home() {
 
               <div className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-sm bg-ternary text-white">
+                  <div className="absolute flex items-center justify-center w-12 h-12 text-white rounded-sm bg-ternary">
                     {/* <!-- Heroicon name: outline/scale --> */}
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
@@ -73,7 +74,7 @@ function Home() {
                       />
                     </svg>
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Fini la paperasse</p>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Fini la paperasse</p>
                 </dt>
                 <dd className="mt-2 ml-16 text-base text-gray-500">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
@@ -83,9 +84,9 @@ function Home() {
 
               <div className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-sm bg-ternary text-white">
+                  <div className="absolute flex items-center justify-center w-12 h-12 text-white rounded-sm bg-ternary">
                     {/* <!-- Heroicon name: outline/lightning-bolt --> */}
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
@@ -94,7 +95,7 @@ function Home() {
                       />
                     </svg>
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Ne vous ruinez plus</p>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Ne vous ruinez plus</p>
                 </dt>
                 <dd className="mt-2 ml-16 text-base text-gray-500">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
@@ -104,13 +105,13 @@ function Home() {
 
               <div className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-sm bg-ternary text-white">
+                  <div className="absolute flex items-center justify-center w-12 h-12 text-white rounded-sm bg-ternary">
                     {/* <!-- Heroicon name: outline/annotation --> */}
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
                     </svg>
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Vérifiez la réputation de vos contacts</p>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Vérifiez la réputation de vos contacts</p>
                 </dt>
                 <dd className="mt-2 ml-16 text-base text-gray-500">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
@@ -124,7 +125,7 @@ function Home() {
 
       <div className="bg-secondary">
         <div className="grid grid-col-1 md:grid-cols-2">
-          <div className="mx-auto px-4 sm:px-6 md:px-8 pt-24 md:py-24 text-center lg:text-left">
+          <div className="px-4 pt-24 mx-auto text-center sm:px-6 md:px-8 md:py-24 lg:text-left">
             <h1 className="mt-0 mb-8 text-5xl font-extrabold leading-none tracking-tight text-white sm:text-6xl lg:text-7xl sm:mt-7 sm:mb-10">
               Titre inspirant
             </h1>
@@ -134,22 +135,22 @@ function Home() {
             </p>
             <button
               type="button"
-              className="px-5 py-3 text-base font-medium text-primary border border-transparent rounded-sm bg-white hover:bg-gray-200">
+              className="px-5 py-3 text-base font-medium bg-white border border-transparent rounded-sm text-primary hover:bg-gray-200">
               En savoir plus
             </button>
           </div>
-          <div className="inspire_img relative w-full bg-cover bg-center min-h-hscreen">
-            <div className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-b md:bg-gradient-to-r from-secondary"></div>
+          <div className="inspiring-image">
+            <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b md:bg-gradient-to-r from-secondary"></div>
           </div>
         </div>
       </div>
 
       <div className="bg-white">
-        <div className="container mx-auto px-4 sm:px-6 md:px-8 py-24 text-center">
+        <div className="container px-4 py-24 mx-auto text-center sm:px-6 md:px-8">
           <h1 className="mt-0 mb-8 text-4xl font-extrabold leading-none tracking-tight text-primary sm:text-5xl lg:text-6xl sm:mt-7 sm:mb-10">
             Titre inspirant
           </h1>
-          <p className="my-3 text-base text-primary sm:my-5 sm:text-lg max-w-2xl mx-auto md:mt-5 md:text-xl">
+          <p className="max-w-2xl mx-auto my-3 text-base text-primary sm:my-5 sm:text-lg md:mt-5 md:text-xl">
             At nulla eaque in voluptate exercitationem? Rem accusamus quasi sit enim nesciunt cum necessitatibus sint aut ipsum voluptas sit
             consequatur inventore aut quia dolor et quia quas
           </p>

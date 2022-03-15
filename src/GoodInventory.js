df3e57f5f0fcf776d3da652c4392d1d1d13d0569
Martial Séron <martial.seron@gmail.com>
2022-03-15 23:29:51 +0000

    Add Information

--- src/GoodInventory.js
+++ src/GoodInventory.js
@@ -38,7 +38,25 @@ function GoodInventory() {
 
   return (
     <form onSubmit={handleSubmitForm}>
-      <div className="grid gap-6 mb-8">
+      <div className="grid gap-y-6 mb-8">
+        <div className="bg-ternary-lighter border-2 border-ternary-light border-t-4 border-t-ternary rounded text-primary-dark px-4 py-3">
+          <div className="flex">
+            <div className="py-1">
+              <svg className="fill-current h-6 w-6 text-ternary mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+              </svg>
+            </div>
+            <div>
+              <p className="font-bold text-xl">Information</p>
+              <p>
+                L&apos;état des lieux doit être établi de façon contradictoire entre les deux parties lors de la remise des clés au locataire et lors
+                de leur restitution en fin de bail, conformément à l&apos;article 3 de la Loi n°89-462 du 6 juillet 1989. Il fait partie du contrat de
+                location, dont il ne peut être dissocié. L&apos;état des lieux d&apos;entrée peut être complété par le locataire dans les 10 jours
+                suivant sa tenue (et pendant le 1er mois de chauffe pour les éléments de chauffage)
+              </p>
+            </div>
+          </div>
+        </div>
         <Card title="Informations générales" id="informations_generales" className="md:col-span-2">
           <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mx-12">
             <div className="form-component lg:col-span-2">

82a13935c00c5ce67577bf7b725586df6263dff0
Martial Séron <martial.seron@gmail.com>
2022-09-26 22:35:19 +0000

    improve SimpleModal

--- src/SimpleModal.js
+++ src/SimpleModal.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 
-function SimpleModal({ title = '', content = '', confirmButtonText = 'OK', showModal = false, onClose, size = 'md' }) {
+function SimpleModal({ title = '', content = '', confirmButtonText = 'OK', showModal = false, onClose, size = 'md', children }) {
   return (
     <>
       {!showModal ? null : (
         <>
           <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
-            <div className={`relative w-full max-w-${size} mx-auto my-6`}>
+            <div className={`relative max-w-${size} mx-auto my-6`}>
               {/*content*/}
               <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
                 {/*header*/}
@@ -21,14 +21,11 @@ function SimpleModal({ title = '', content = '', confirmButtonText = 'OK', showM
                 {/*body*/}
                 <div className="relative flex-auto p-6 ">
                   {/* <p className="my-4 text-lg leading-relaxed text-blueGray-500">{content}</p> */}
-                  <div className="overflow-y-auto h-modal">{content}</div>
+                  <div className="overflow-y-auto h-modal">{content || children}</div>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
-                  <button
-                    className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
-                    type="button"
-                    onClick={onClose}>
+                  <button className="btn btn-secondary" type="button" onClick={onClose}>
                     {confirmButtonText}
                   </button>
                 </div>
@@ -51,4 +48,5 @@ SimpleModal.propTypes = {
   content: PropTypes.string,
   size: PropTypes.oneOf(['sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl']),
   confirmButtonText: PropTypes.string,
+  children: PropTypes.any,
 };
